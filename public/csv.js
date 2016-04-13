0dd99afdba4237b1b55387a510fc8fb1482058f0
Ruymán Rodríguez Martín <alu0100845235@ull.edu.es>
2016-04-13 23:48:53 +0100

    Creada funciones ajax en app.js y csv.js

--- public/csv.js
+++ public/csv.js
@@ -73,18 +73,21 @@ $(document).ready(() => {
     }
 
     /* Request AJAX para que se calcule la tabla */
-    XXXXXXXXXXXXXXXXXX XX XX X
-        XX XXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXX X XXXXXXXXXXXXXXX
-        XXXXXXXXXXXXX 
-          X XXXXXX XXXXXXXXXXXXXX XX 
-          XXXXXXXXXX
-          XXXXXX
-        XX
-   XXX
+    $("#parse").click( () => {
+        if (window.localStorage) localStorage.original = original.value;
+        $.get("/csv",
+          { textocsv: original.value },
+          fillTable,
+          'json'
+        );
+    });
+    
    /* botones para rellenar el textarea */
-   XXXXXXXXXXXXXXXXXXXXXXXXX XXXXX XX X
-     XXXXXXXXXXX XX XX X XXXXXXXXXXXXXXXXXXXXXXXXXXX XXX
-   XXX
+   $('button.example').each((index, element) => {
+     $(element).click(() => { 
+        dump(`${$(element).text()}.txt`); 
+      });
+   });
 
     // Setup the drag and drop listeners.
     //var dropZone = document.getElementsByClassName('drop_zone')[0];
